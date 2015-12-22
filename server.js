3f13bcf029392d2c60911ff57654d77f764c0a6f
Marek Wojtaszek <kontakt@marekwojtaszek.com>
2015-12-22 09:35:08 +0100

    Added default envs

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
-var connection  = MongoClient.connect(process.env.MONGOLAB_URI);
+var connection  = MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/bookinventory');
 var dataSource  = require('./db.js')(connection);
 var mock        = require('./mock')();
 
 var app = require('./app.js')(dataSource);
 
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
