77fd1623e58fee027cdeb115dff7d2dd2e907f3e
Marek Wojtaszek <kontakt@marekwojtaszek.com>
2015-12-22 14:39:09 +0100

    Logging errors

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ module.exports = function(dataSource) {
 
   function serverError(err, req, res, next) {
     res.status(err.status || 500);
+    console.error(err.stack);
     res.json({
       message: err.message,
       error: (process.env.NODE_ENV === 'production') ? {} : err
