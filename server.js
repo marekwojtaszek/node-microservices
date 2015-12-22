f283418afa8746ccaa645eb8d193055d845b22b7
Marek Wojtaszek <kontakt@marekwojtaszek.com>
2015-12-22 09:32:06 +0100

    Replaced hardcoded values with env variables

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
-var connection  = MongoClient.connect('mongodb://localhost:27017/bookinventory');
+var connection  = MongoClient.connect(process.env.MONGOLAB_URI);
 var dataSource  = require('./db.js')(connection);
 var mock        = require('./mock')();
 
 var app = require('./app.js')(dataSource);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
 
